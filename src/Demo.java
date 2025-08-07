569bedcad8693d6fe0ad96bbe71ef717c423f33f
saurabhtomar52 <saurshingh123@gmail.com>
2025-08-07 20:57:19 +0530

    adding some more things

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,28 @@
 
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+// public class Demo {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, GitHub!");
+//     }
+// }
+// class: it is a usure define blueprint configaring atribute & method
+// object: intance of a class
+// public class Demo{
+
+//     public static void main(String[] args){
+//         System.out.println("hello world!");
+//         System.out.print("hello world!");
+//     }
+// }
+// print for disier output
+// println for next line print
+// \n for next line print
+
+public class Demo{
+
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.print("****");
     }
 }
